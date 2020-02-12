1724b18535aa8b297408edef69a6fe86ed537583
Bishal Peter Dores <bishalpd@gmail.com>
2020-02-12 13:44:08 +0600

    register form

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
